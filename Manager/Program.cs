5450029f9b4889709a46c70bce590cad136280f0
Rodrigo Díaz Concha <rodrigo@rdiazconcha.com>
2020-01-22 23:24:59 -0600

    Autenticación

--- Manager/Program.cs
+++ Manager/Program.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Threading.Tasks;
 
 namespace Manager
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var token = await GetTokenAsync();
+            Console.WriteLine(token);
+        }
+
+        static async Task<string> GetTokenAsync()
+        {
+            var authContext = new AuthenticationContext(Settings.Authority);
+            var deviceCodeResult = await
+                authContext.AcquireDeviceCodeAsync(Settings.DigitalTwinsResourceId, Settings.ClientId);
+            Console.WriteLine(deviceCodeResult.Message);
+            var token = await authContext.AcquireTokenByDeviceCodeAsync(deviceCodeResult);
+            return token.AccessToken;
         }
     }
 }
